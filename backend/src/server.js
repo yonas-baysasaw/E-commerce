f81a219bd8e557861910d5fea07fbb169f38dd49
yonas-baysasaw <yonas.baysasaw.kibret@gmail.com>
2025-12-08 16:20:59 +0300

    inital commit

---
+++ backend/src/server.js
@@ -0,0 +1,6 @@
+import express from 'express'
+const app = express()
+app.get("/api/health", (req, res) =>{
+    res.status(200).json({message : "Success"})
+})
+app.listen(3000,() => console.log("start a server 132"))
