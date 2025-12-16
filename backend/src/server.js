78143becb72d066db2c1640180fc6f07ad43c73a
yonas-baysasaw <yonas.baysasaw.kibret@gmail.com>
2025-12-16 18:30:08 +0300

    git add the auth setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,42 +1,40 @@
 import express from "express";
-import path from 'path'
-import { ENV } from "./config/env.js";
-import { connectDB } from "./config/db.js";
-import { clerkMiddleware } from '@clerk/express';
-import {serve} from "inngest/express";
+import path from "path";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+
 import { functions, inngest } from "./config/inngest.js";
 
-
-
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
-
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 app.use(express.json());
-// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use(clerkMiddleware()); // adds auth object under the req => req.auth
+
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-
-
-
-app.use(clerkMiddleware()) // adds auth object under req
-
-app.get("/api/health", (_req, res) => {
-    res.status(200).json({ message: "the server is running" });
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "Success" });
 });
-if(ENV.NODE_ENV == 'production'){
-    app.use(express.static(path.join(__dirname, "../admin/dist")))
-    app.get("/{*any}", (req, res)=>{
-        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"))
-    })
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
 }
+
 const startServer = async () => {
-    await connectDB();
-    app.listen(ENV.PORT, () => {
-        console.log("Server is up and running");
-    })
-}
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log("Server is up and running");
+  });
+};
 
 startServer();
